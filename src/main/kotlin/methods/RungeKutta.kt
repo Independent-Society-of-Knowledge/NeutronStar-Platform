17a1a4edab2bc2b352626c897457618843135547
Kid A <thisismeamir@outlook.com>
2024-10-10 20:34:32 +0330

    [Implementation] RungeKutta.kt based on new Point.kt data class

--- src/main/kotlin/methods/RungeKutta.kt
+++ src/main/kotlin/methods/RungeKutta.kt
@@ -1,26 +1,50 @@
 package methods
 
-//import
-//fun rungeKutta(stepSize: Double, initialValues: Point, equationOfMotion: (Point) -> Point): Sequence<Point> =
-//    sequence<Point> {
-//
-//        var point = initialValues.copy()
-//        val stepSize2 = stepSize / 2.0
-//
-//        yield(point.copy())
-//
-//        while (true) {
-//
-//            val k1 = equationOfMotion(point)
-//            val k2 = equationOfMotion(point + k1 * stepSize2)
-//            val k3 = equationOfMotion(point + k2 * stepSize2)
-//            val k4 = equationOfMotion(point + k3 * stepSize)
-//            point += (k1 +  k2 * 2.0 +  k3 * 2.0 + k4) * (stepSize / 6.0)
-//            point.radius += stepSize
-//
-//            yield(point.copy())
-//
-//        }
-//    }
+import Point
+import visual.plotPairSequence
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.sin
 
+fun rungeKutta(stepSize: Double, initialValues: Point, equationOfMotion: (Point) -> Point): Sequence<Point> =
+    sequence<Point> {
 
+        var point = initialValues.copy()
+        val stepSize2 = stepSize / 2.0
+
+        yield(point.copy())
+
+        while (true) {
+            val radius = point.radius
+            val k1 = equationOfMotion(point)
+            val k2 = equationOfMotion(point + k1 * stepSize2)
+            val k3 = equationOfMotion(point + k2 * stepSize2)
+            val k4 = equationOfMotion(point + k3 * stepSize)
+            point += (k1 + k2 * 2.0 + k3 * 2.0 + k4) * (stepSize / 6.0)
+            point.radius = radius + stepSize
+
+            yield(point.copy())
+
+        }
+    }
+
+//fun main() {
+//    rungeKutta(0.1, Point(
+//        pressure = 1.0,
+//        mass = 0.0,
+//        radius = 0.01,
+//        density = 0.0,
+//        baryonDensity = 0.0
+//    ), {
+//        Point(
+//            pressure =  -sin(it.radius) ,
+//            mass = cos(it.radius),
+//            radius = 1.0 ,
+//            density = 0.0,
+//            baryonDensity = 0.0
+//        )
+//    }).take(100)
+//        .map { it.radius to it.pressure }
+//        .plotPairSequence()
+//}
---
+++ src/main/kotlin/visual/PlotPairSequence.kt
@@ -0,0 +1,32 @@
+package visual
+
+import org.jetbrains.letsPlot.export.ggsave
+import org.jetbrains.letsPlot.geom.geomLine
+import org.jetbrains.letsPlot.ggsize
+import org.jetbrains.letsPlot.label.ggtitle
+import org.jetbrains.letsPlot.letsPlot
+import org.jetbrains.letsPlot.themes.themeMinimal
+
+fun Sequence<Pair<Double, Double>>.plotPairSequence(name: String = "Plot", xLabel: String = "x", yLabel: String = "y") {
+    val xList = this.map { it.first }
+    val yList = this.map { it.second }
+    // Create a data map for plotting
+    val data = mapOf<String, Any>(
+        xLabel to xList,
+        yLabel to yList
+    )
+
+    // Create the plot
+    val p = letsPlot(data)
+    p + ggsize(1920, 1080) + ggtitle(name) + themeMinimal()
+    val plt = p + geomLine { x = xLabel; y = yLabel }
+
+
+    // Example: Save to file or show depending on environment
+    ggsave(plt, "plot_${name}.svg")  // Saves the plot to a PNG file
+
+    // Uncomment the following line for JavaFX display if using JavaFX
+    // p.show()
+
+}
+
